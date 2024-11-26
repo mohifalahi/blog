b7bf0ee0e1a17d309bf38bc2f2f433f7c1fdfd65
Mohammad Falahi <45416720+mohifalahi@users.noreply.github.com>
2024-11-26 21:22:37 +0330

    tests: view_tests added, some changes in ad serializer and views for tests (#3)

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('ads/new/', views.AdCreateAPIView.as_view(), name='ad-create'),
     path('ads/', views.AdListAPIView.as_view(), name='ad-list'),
-    path('ads/<int:pk>/', views.AdDetailAPIView.as_view(), name='ad-detail'),
+    path('ads/update/<int:pk>/', views.AdUpdateAPIView.as_view(), name='ad-update'),
+    path('ads/detail/<int:pk>/', views.AdDetailDestroyAPIView.as_view(), name='ad-detail'),
+    path('ads/delete/<int:pk>/', views.AdDetailDestroyAPIView.as_view(), name='ad-delete'),
 
     path('comments/new/', views.CommentCreateAPIView.as_view(), name='comment-create'),
 ]
