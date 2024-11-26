afdc337ac762054b0e658c5992bfd342310eee75
Mohammad Falahi <45416720+mohifalahi@users.noreply.github.com>
2024-11-26 11:42:47 +0330

    feat: add ad and comment logic (#2)

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    
+    path('ads/new/', views.AdCreateAPIView.as_view(), name='ad-create'),
+    path('ads/', views.AdListAPIView.as_view(), name='ad-list'),
+    path('ads/<int:pk>/', views.AdDetailAPIView.as_view(), name='ad-detail'),
+
+    path('comments/new/', views.CommentCreateAPIView.as_view(), name='comment-create'),
 ]
